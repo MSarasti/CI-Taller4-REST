4eb73abef1339aa2e056dea8090f077e5a390212
MSarasti <54419255+MSarasti@users.noreply.github.com>
2021-12-16 08:46:45 -0500

    Updated operator controller in the frontend

--- src/main/java/com/taller4/frontend/controller/OperatorControllerImpl.java
+++ src/main/java/com/taller4/frontend/controller/OperatorControllerImpl.java
@@ -1,7 +1,5 @@
 package com.taller4.frontend.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -48,145 +46,145 @@ public class OperatorControllerImpl {
     }
 	
 	@GetMapping("/specialoffer/add")
-	public String addSO(Model model) {
+	public String addSOGet(Model model) {
 		model.addAttribute("specialoffer", new Specialoffer());
 		return "operator/addSO";
 	}
 	
 	@PostMapping("/specialoffer/add")
-	public String saveSO(@Validated({ addValidation.class }) @ModelAttribute("specialoffer") Specialoffer specialoffer, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+	public String addSOPost(@Validated({ addValidation.class }) @ModelAttribute("specialoffer") Specialoffer specialoffer, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
 		if(!action.equals("Cancel")) {
 			if(bindingResult.hasErrors()) {
 				model.addAttribute("specialoffer", specialoffer);
 				return "operator/addSO";
 			}
-			soService.saveSpecialOffer(specialoffer);
+			bDelegate.soSave(specialoffer);
 		}
 		return "redirect:/specialoffer/";
 	}
 	
 	@GetMapping("/specialofferproduct/add")
-	public String addSOP(Model model) {
+	public String addSOPGet(Model model) {
 		model.addAttribute("specialofferproduct", new Specialofferproduct());
-		model.addAttribute("offers", soService.findAll());
-		model.addAttribute("products", pService.findAllProducts());
+		model.addAttribute("offers", bDelegate.soFindAll());
+		model.addAttribute("products", bDelegate.prodFindAll());
 		return "operator/addSOP";
 	}
 	
 	@PostMapping("/specialofferproduct/add")
-	public String saveSOP(@Validated({ addValidation.class }) @ModelAttribute("specialofferproduct") Specialofferproduct specialofferproduct, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+	public String addSOPPost(@Validated({ addValidation.class }) @ModelAttribute("specialofferproduct") Specialofferproduct specialofferproduct, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
 		if(!action.equals("Cancel")) {
 			if(bindingResult.hasErrors()) {
 				model.addAttribute("specialofferproduct", specialofferproduct);
-				model.addAttribute("offers", soService.findAll());
-				model.addAttribute("products", pService.findAllProducts());
+				model.addAttribute("offers", bDelegate.soFindAll());
+				model.addAttribute("products", bDelegate.prodFindAll());
 				return "operator/addSOP";
 			}
-			sopService.saveSpecialOfferProduct(specialofferproduct, specialofferproduct.getpId(), specialofferproduct.getsId());
+			bDelegate.sopSave(specialofferproduct);
 		}
 		return "redirect:/specialofferproduct/";
 	}
 	
 	@GetMapping("/salesorderdetail/add")
-	public String addSOD(Model model) {
+	public String addSODGet(Model model) {
 		model.addAttribute("specialofferproduct", new Specialofferproduct());
-		model.addAttribute("offers", soService.findAll());
-		model.addAttribute("products", pService.findAllProducts());
+		model.addAttribute("offers", bDelegate.soFindAll());
+		model.addAttribute("products", bDelegate.prodFindAll());
 		return "operator/addSOD";
 	}
 	
 	@PostMapping("/salesorderdetail/add")
-	public String saveSOD(@Validated({ addValidation.class }) @ModelAttribute("salesorderdetail") Salesorderdetail salesorderdetail, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+	public String addSODPost(@Validated({ addValidation.class }) @ModelAttribute("salesorderdetail") Salesorderdetail salesorderdetail, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
 		if(!action.equals("Cancel")) {
 			if(bindingResult.hasErrors()) {
 				model.addAttribute("salesorderdetail", salesorderdetail);
-				model.addAttribute("offers", soService.findAll());
-				model.addAttribute("products", pService.findAllProducts());
+				model.addAttribute("offers", bDelegate.soFindAll());
+				model.addAttribute("products", bDelegate.prodFindAll());
 				return "operator/addSOD";
 			}
-			sodService.saveSalesOrderDetail(salesorderdetail, salesorderdetail.getpId(), salesorderdetail.getsoId());
+			bDelegate.sodSave(salesorderdetail);
 		}
 		return "redirect:/salesorderdetail/";
 	}
 	
 	@GetMapping("/specialoffer/edit/{id}")
-	public String editSO(@PathVariable("id") Integer id, Model model) {
-		Specialoffer so = soService.findById(id);
+	public String editSOGet(@PathVariable("id") Integer id, Model model) {
+		Specialoffer so = bDelegate.soFindById(id);
 		
 		model.addAttribute("specialoffer", so);
 		return "operator/editSO";
 	}
 	
 	@PostMapping("/specialoffer/edit/{id}")
-	public String updateSO(@PathVariable("id") Integer id, @Validated(updateValidation.class) Specialoffer specialoffer, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+	public String editSOPost(@PathVariable("id") Integer id, @Validated(updateValidation.class) @ModelAttribute("specialoffer") Specialoffer specialoffer, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
 		if (!action.equals("Cancel")) {
 			if(bindingResult.hasErrors()) {
-				model.addAttribute("specialoffer", soService.findById(id));
+				model.addAttribute("specialoffer", bDelegate.soFindById(id));
 				return "operator/editSO";
 			}
 			specialoffer.setSpecialofferid(id);
-			soService.updateSpecialOffer(id, specialoffer);
+			bDelegate.soUpdate(specialoffer);
 		}
 		return "redirect:/specialoffer";
 	}
 	
 	@GetMapping("/specialofferproduct/edit/{id}")
-	public String editSOP(@PathVariable("id") SpecialofferproductPK id, Model model) {
-		Specialofferproduct so = sopService.findById(id);
+	public String editSOPGet(@PathVariable("id") SpecialofferproductPK id, Model model) {
+		Specialofferproduct sop = bDelegate.sopFindById(id);
 		
-		model.addAttribute("specialoffer", so);
+		model.addAttribute("specialofferproduct", sop);
 		return "operator/editSOP";
 	}
 	
 	@PostMapping("/specialofferproduct/edit/{id}")
-	public String updateSOP(@PathVariable("id") SpecialofferproductPK id, @Validated(updateValidation.class) Specialofferproduct specialofferproduct, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+	public String editSOPPost(@PathVariable("id") SpecialofferproductPK id, @Validated(updateValidation.class) @ModelAttribute("specialofferproduct") Specialofferproduct specialofferproduct, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
 		if (!action.equals("Cancel")) {
 			if(bindingResult.hasErrors()) {
-				model.addAttribute("specialofferproduct", sopService.findById(id));
+				model.addAttribute("specialofferproduct", bDelegate.sopFindById(id));
 				return "operator/editSOP";
 			}
 			specialofferproduct.setId(id);
-			sopService.updateSpecialOfferProduct(id, specialofferproduct);
+			bDelegate.sopUpdate(specialofferproduct);
 		}
 		return "redirect:/specialofferproduct";
 	}
 	
 	@GetMapping("/salesorderdetail/edit/{id}")
-	public String editSOD(@PathVariable("id") Integer id, Model model) {
-		Salesorderdetail so = sodService.findById(id);
+	public String editSODGet(@PathVariable("id") Integer id, Model model) {
+		Salesorderdetail sod = bDelegate.sodFindById(id);
 		
-		model.addAttribute("salesorderdetail", so);
+		model.addAttribute("salesorderdetail", sod);
 		return "operator/editSOD";
 	}
 	
 	@PostMapping("/salesorderdetail/edit/{id}")
-	public String updateSOD(@PathVariable("id") Integer id, @Validated(updateValidation.class) Salesorderdetail salesorderdetail, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
+	public String editSODPost(@PathVariable("id") Integer id, @Validated(updateValidation.class) @ModelAttribute("salesorderdetail") Salesorderdetail salesorderdetail, BindingResult bindingResult, Model model, @RequestParam(value = "action", required = true) String action) {
 		if (!action.equals("Cancel")) {
 			if(bindingResult.hasErrors()) {
-				model.addAttribute("salesorderdetail", sodService.findById(id));
+				model.addAttribute("salesorderdetail", bDelegate.sodFindById(id));
 				return "operator/editSOD";
 			}
 			salesorderdetail.setSalesOrderDetailId(id);
-			sodService.updateSalesOrderDetail(id, salesorderdetail);
+			bDelegate.sodUpdate(salesorderdetail);
 		}
 		return "redirect:/salesorderdetail";
 	}
 	
 	@GetMapping("/specialoffer/delete/{id}")
 	public String deleteSO(@PathVariable("id") Integer id, Model model) {
-		soService.deleteSpecialOffer(id);
+		bDelegate.soDelete(id);
 		return "redirect:/specialoffer";
 	}
 	
 	@GetMapping("/specialofferproduct/delete/{id}")
 	public String deleteSOP(@PathVariable("id") SpecialofferproductPK id, Model model) {
-		sopService.deleteSpecialOfferProduct(id);
+		bDelegate.sopDelete(id);
 		return "redirect:/specialofferproduct";
 	}
 	
 	@GetMapping("/salesorderdetail/delete/{id}")
 	public String deleteSOD(@PathVariable("id") Integer id, Model model) {
-		sodService.deleteSalesOrderDetail(id);
+		bDelegate.sodDelete(id);
 		return "redirect:/salesorderdetail";
 	}
 }
